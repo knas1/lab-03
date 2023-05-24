9cddad9eb999752d6577e23598469a9c4b2e19e3
knas1 <83686663+knas1@users.noreply.github.com>
2023-05-24 16:49:36 +0300

    Update Lab3Test.java

--- src/test/java/edu/kau/fcit/cpit252/Lab3Test.java
+++ src/test/java/edu/kau/fcit/cpit252/Lab3Test.java
@@ -1,7 +1,7 @@
 package edu.kau.fcit.cpit252;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.Constructor;
 
